3f05eb8654ee6703e05c979d310be900f71de100
Konstantin Burkalev <KSDaemon@yandex.ru>
2014-03-16 00:58:53 +0400

    Created a backbone for lib

---
+++ src/wiola.init.lua
@@ -0,0 +1,5 @@
+--
+-- Project: this project
+-- User: kostik
+-- Date: 16.03.14
+--
---
+++ src/wiola.lua
@@ -0,0 +1,16 @@
+--
+-- Project: this project
+-- User: kostik
+-- Date: 16.03.14
+--
+
+local _M = {
+	_VERSION = '0.1'
+}
+
+local cjson = require "cjson"
+local mp = require 'MessagePack'
+
+
+
+return _M
