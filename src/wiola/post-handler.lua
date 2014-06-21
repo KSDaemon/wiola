b17b24430881dcc2d4577de20dbea7d9290fbbdd
Konstantin Burkalev <KSDaemon@yandex.ru>
2014-06-21 14:24:10 +0400

    Edit docs for POST processing

--- src/wiola/post-handler.lua
+++ src/wiola/post-handler.lua
@@ -5,7 +5,7 @@
 --
 
 local wampServer = require "wiola"
-local realm = "hga"
+local realm = "testRealm"
 
 local redisOk, redisErr = wampServer.setupRedis("unix:/tmp/redis.sock")
 if not redisOk then
@@ -13,14 +13,13 @@ if not redisOk then
 	return ngx.exit(444)
 end
 
---local req = ngx.var.request_body
 ngx.req.read_body()
 local req = ngx.req.get_body_data()
 
 local res, httpCode = wampServer.processPostData(ngx.var.connection, realm, req)
 
 ngx.status = httpCode
-ngx.say(res)
+ngx.say(res)    -- returns response to client
 
 -- to cause quit the whole request rather than the current phase handler
 ngx.exit(ngx.HTTP_OK)
