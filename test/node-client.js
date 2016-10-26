592e10a822d8e67fa778d986f9ab3044bc494310
Konstantin Burkalev <KSDaemon@yandex.ru>
2016-10-26 18:21:29 +0300

    Added small wampy/node.js test example

---
+++ test/node-client.js
@@ -0,0 +1,39 @@
+/**
+ * Project: wiola
+ * User: KSDaemon
+ * Date: 21.10.16
+ */
+
+const Wampy = require('wampy');
+const w3cws = require('websocket').w3cwebsocket;
+let ws, d = Date.now();
+
+console.log('0: Initializing wampy and connecting to server...');
+
+ws = new Wampy('ws://webxp/ws/', {
+    ws: w3cws,
+    realm: 'test',
+    onConnect: function () {
+        console.log('+' + (Date.now() - d) + 'ms: Yahoo! We are online!');
+        ws.subscribe('message.received', {
+           onSuccess: function () {
+               console.log('+' + (Date.now() - d) + 'ms: Successfully subscribed to topic');
+               ws.publish('message.received', ['New message'], null, { exclude_me: false });
+
+           },
+           onError: function (err, details) { console.log('+' + (Date.now() - d) + 'ms: Subscription error:' + err); },
+           onEvent: function (arrayPayload, objectPayload) {
+               console.log('+' + (Date.now() - d) + 'ms: Received new message!');
+               console.log('+' + (Date.now() - d) + 'ms: Closing connection...');
+               ws.disconnect();
+           }
+        });
+
+    },
+    onClose: function () {
+        console.log('+' + (Date.now() - d) + 'ms: Connection to WAMP server closed!');
+    },
+    onError: function () { console.log('Breakdown happened'); },
+    onReconnect: function () { console.log('Reconnecting...'); },
+    onReconnectSuccess: function () { console.log('Reconnection succeeded...'); }
+});
