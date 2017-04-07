94f9609f011d36865c4ccbf97eba9c1f5a3d35c1
Konstantin Burkalev <KSDaemon@yandex.ru>
2017-04-07 15:46:54 +0300

    Added module for redis store flushing

---
+++ src/wiola/flushdb.lua
@@ -0,0 +1,30 @@
+--
+-- Project: wiola
+-- User: Konstantin Burkalev
+-- Date: 06.04.17
+--
+
+local _M = {}
+
+--
+-- Cleans up all wiola sessions data in redis store
+--
+-- redis - Redis instance on which to operate
+--
+function _M.flushAll()
+    ngx.log(ngx.DEBUG, "Cleaning up all wiola sessions...")
+
+    local conf = require("wiola.config").config()
+    local redis = require "redis"
+    local client = redis.connect(conf.redis.host, conf.redis.port)
+
+    if conf.redis.db then
+        client:select(conf.redis.db)
+    end
+
+    client:flushdb()
+
+    ngx.log(ngx.DEBUG, "Sessions data successfully cleared!")
+end
+
+return _M
