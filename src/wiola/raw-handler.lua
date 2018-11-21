80808b059ec4345c3a698bfbd2101f0980af1cba
Konstantin Burkalev <KSDaemon@yandex.ru>
2018-11-21 16:13:39 +0300

    Debugged and refactored raw handler

--- src/wiola/raw-handler.lua
+++ src/wiola/raw-handler.lua
@@ -32,32 +32,32 @@ tcpSocket, err = ngx.req.socket(true)
 
 if not tcpSocket then
     ngx.log(ngx.ERR, "Failed to initialize downstream socket: ", err)
-    return ngx.exit(444)
+    return ngx.exit(ngx.ERROR)
 end
 
-tcpSocket:settimeout(config.socketTimeout)
+tcpSocket:settimeouts(config.socketTimeout, config.socketTimeout, config.socketTimeout)
 
 wampServer, err = wiola:new()
 if not wampServer then
     ngx.log(ngx.DEBUG, "Failed to create a wiola instance: ", err)
-    return ngx.exit(444)
+    return ngx.exit(ngx.ERROR)
 end
 
 data, err = tcpSocket:receive(4)
 
 if data == nil then
     ngx.log(ngx.ERR, "Failed to receive data: ", err)
-    return ngx.exit(444)    -- tcpSocket:close()
+    return ngx.exit(ngx.ERROR)
 end
 
 if string.byte(data) ~= 0x7F then
     ngx.log(ngx.ERR, "Can not recognize WAMP handshake byte sequence")
-    return ngx.exit(444)
+    return ngx.exit(ngx.ERROR)
 elseif string.byte(data, 3) ~= 0x0 or string.byte(data, 4) ~= 0x0 then
     ngx.log(ngx.ERR, "Reserved WAMP handshake bytes are not 0")
     cliData = string.char(0x7F, bit.bor(bit.lshift(3, 4), 0), 0, 0)
     tcpSocket:send(cliData)
-    return ngx.exit(444)
+    return ngx.exit(ngx.ERROR)
 end
 
 cliMaxLength = math.pow(2, 9 + bit.rshift(string.byte(data, 2), 4))
@@ -71,7 +71,7 @@ else
     ngx.log(ngx.ERR, "Can not recognize serializer to use (", serializer, ")")
     cliData = string.char(0x7F, bit.bor(bit.lshift(1, 4), 0), 0, 0)
     tcpSocket:send(cliData)
-    return ngx.exit(444)
+    return ngx.exit(ngx.ERROR)
 end
 
 local sessionId, dataType = wampServer:addConnection(ngx.var.connection, serializerStr)
@@ -83,11 +83,10 @@ data, err = tcpSocket:send(cliData)
 
 if not data then
     ngx.log(ngx.ERR, "Failed to send handshake data: ", err)
-    return ngx.exit(444)
+    return ngx.exit(ngx.ERROR)
 end
 
 local function removeConnection(_, sessId)
-
     ngx.log(ngx.DEBUG, "Cleaning up session: ", sessId)
 
     local wconfig = require("wiola.config").config()
@@ -110,7 +109,7 @@ end
 ok, err = ngx.on_abort(removeConnectionWrapper)
 if not ok then
     ngx.log(ngx.ERR, "failed to register the on_abort callback: ", err)
-    ngx.exit(444)
+    ngx.exit(ngx.ERROR)
 end
 
 local function getLenBytes(len)
@@ -123,37 +122,18 @@ local function getLenBytes(len)
 end
 
 while true do
---    ngx.log(ngx.DEBUG, "Started handler loop!")
     local hflags, msgType, msgLen
 
+    --    ngx.log(ngx.DEBUG, "Checking data for client...")
     hflags = wampServer:getHandlerFlags(sessionId)
-    if hflags ~= nil then
-        if hflags.sendLast == true then
-            cliData = wampServer:getPendingData(sessionId, true)
+    cliData = wampServer:getPendingData(sessionId, hflags.sendLast)
 
-            data, err = tcpSocket:send(cliData)
-
-            if not data then
-                ngx.log(ngx.ERR, "Failed to send data: ", err)
-            end
-        end
-
-        if hflags.close == true then
-            ngx.log(ngx.DEBUG, "Got close connection flag for session")
-            ngx.timer.at(0, removeConnection, sessionId)
-            return ngx.exit(444)
-        end
-    end
-
---    ngx.log(ngx.DEBUG, "Checking data for client...")
-    cliData = wampServer:getPendingData(sessionId)
-
-    while cliData ~= ngx.null do
+    if cliData ~= ngx.null and cliData then
 
         msgLen = string.len(cliData)
 
         if msgLen < cliMaxLength then
-            ngx.log(ngx.DEBUG, "Got data for client. DataType is ", dataType, ". Sending...")
+            ngx.log(ngx.DEBUG, "Got data for client. DataType is ", dataType, ". Data: ", cliData, ". Sending...")
             cliData = string.char(0) .. getLenBytes(msgLen) .. cliData
             data, err = tcpSocket:send(cliData)
 
@@ -163,56 +143,56 @@ while true do
         end
 
         -- TODO Handle exceeded message length situation
+    end
 
-        cliData = wampServer:getPendingData(sessionId)
+    if hflags.close == true then
+        ngx.log(ngx.DEBUG, "Got close connection flag for session")
+        tcpSocket:shutdown("send")
+        ngx.timer.at(0, removeConnection, sessionId)
+        ngx.exit(ngx.OK)
     end
 
     data, err = tcpSocket:receive(4)
 
-    if data == nil then
-        ngx.log(ngx.ERR, "Failed to receive data: ", err)
+    if data ~= nil then
+        msgType = bit.band(string.byte(data), 0xff)
+        msgLen = bit.lshift(string.byte(data, 2), 16) +
+                bit.lshift(string.byte(data, 3), 8) +
+                string.byte(data, 4)
+
+        if msgType == 0 then    -- regular WAMP message
+
+            data, err = tcpSocket:receive(msgLen)
+
+            if data == nil then
+                ngx.log(ngx.ERR, "Failed to receive data: ", err)
+                ngx.timer.at(0, removeConnection, sessionId)
+                ngx.exit(ngx.ERROR)
+            end
+
+            ngx.log(ngx.DEBUG, "Received client data: ", data)
+            wampServer:receiveData(sessionId, data)
+
+        elseif msgType == 1 then    -- PING
+
+            data, err = tcpSocket:receive(msgLen)
+
+            if data == nil then
+                ngx.log(ngx.ERR, "Failed to receive data: ", err)
+                ngx.timer.at(0, removeConnection, sessionId)
+                ngx.exit(ngx.ERROR)
+            end
+
+            cliData = string.char(2) .. msgLen .. data
+            data, err = tcpSocket:send(cliData)
+
+            if not data then
+                ngx.log(ngx.ERR, "Failed to send data: ", err)
+            end
+        end
+    elseif err == 'closed' then
+        ngx.log(ngx.DEBUG, "Error reading socket: ", err)
         ngx.timer.at(0, removeConnection, sessionId)
-        return ngx.exit(444)
+        ngx.exit(ngx.ERROR)
     end
-
-    msgType = bit.band(string.byte(data), 0xff)
-    msgLen = bit.lshift(string.byte(data, 2), 16) +
-            bit.lshift(string.byte(data, 3), 8) +
-            string.byte(data, 4)
-
-    if msgType == 0 then    -- regular WAMP message
-
-        data, err = tcpSocket:receive(msgLen)
-
-        if data == nil then
-            ngx.log(ngx.ERR, "Failed to receive data: ", err)
-            ngx.timer.at(0, removeConnection, sessionId)
-            return ngx.exit(444)
-        end
-
-        wampServer:receiveData(sessionId, data)
-
-    elseif msgType == 1 then    -- PING
-
-        data, err = tcpSocket:receive(msgLen)
-
-        if data == nil then
-            ngx.log(ngx.ERR, "Failed to receive data: ", err)
-            ngx.timer.at(0, removeConnection, sessionId)
-            return ngx.exit(444)
-        end
-
-        cliData = string.char(2) .. msgLen .. data
-        data, err = tcpSocket:send(cliData)
-
-        if not data then
-            ngx.log(ngx.ERR, "Failed to send data: ", err)
-        end
-
---    elseif msgType == 2 then    -- PONG
-        -- TODO Implement server initiated ping
-
-    end
-
---    ngx.log(ngx.DEBUG, "Finished handler loop!")
 end
