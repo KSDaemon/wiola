5c5b1f90b20f2651c7dade3df5c3f543945ebf6b
Konstantin Burkalev <KSDaemon@ya.ru>
2019-10-22 17:35:28 +0300

    Refactored grunt task to create correct lib/ tree structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,26 +5,46 @@
  */
 
 module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt);
+
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        clean: {
+            dist: ['lib/*']
+        },
         copy: {
             main: {
-                expand: true,
-                flatten: true,
-                src: 'src/wiola/**/*.lua',
-                dest: 'lib/',
-                options: {
-                    process: function (content, srcpath) {
-                        return content.replace(/\s*(\-\-)?\s*ngx\.log.*/g,'')
-                            .replace(/.*getdump.*/g,'');
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: 'src/wiola/wiola.lua',
+                        dest: 'lib/',
+                        options: {
+                            process: function (content, srcpath) {
+                                return content.replace(/\s*(\-\-)?\s*ngx\.log.*/g,'')
+                                    .replace(/.*getdump.*/g,'');
+                            }
+                        }
+                    },
+                    {
+                        expand: true,
+                        cwd: 'src/wiola/',
+                        src: '**/*.lua',
+                        dest: 'lib/wiola/',
+                        options: {
+                            process: function (content, srcpath) {
+                                return content.replace(/\s*(\-\-)?\s*ngx\.log.*/g,'')
+                                    .replace(/.*getdump.*/g,'');
+                            }
+                        }
                     }
-                }
+                ]
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-copy');
-
-    grunt.registerTask('default', ['copy']);
+    grunt.registerTask('default', ['clean', 'copy']);
 };
