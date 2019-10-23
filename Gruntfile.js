9a3e3ed1a99e5eefe61f7a73128f76f94ece2f9a
Konstantin Burkalev <KSDaemon@ya.ru>
2019-10-23 10:02:51 +0300

    Fixed error in build script (no cuts of logs) due to bug in grunt-contrib-copy

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,44 +4,40 @@
  * Date: 14.04.14
  */
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     require('load-grunt-tasks')(grunt);
 
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+        pkg  : grunt.file.readJSON('package.json'),
         clean: {
             dist: ['lib/*']
         },
-        copy: {
+        copy : {
             main: {
-                files: [
-                    {
-                        expand: true,
-                        flatten: true,
-                        src: 'src/wiola/wiola.lua',
-                        dest: 'lib/',
-                        options: {
-                            process: function (content, srcpath) {
-                                return content.replace(/\s*(\-\-)?\s*ngx\.log.*/g,'')
-                                    .replace(/.*getdump.*/g,'');
-                            }
-                        }
-                    },
-                    {
-                        expand: true,
-                        cwd: 'src/wiola/',
-                        src: '**/*.lua',
-                        dest: 'lib/wiola/',
-                        options: {
-                            process: function (content, srcpath) {
-                                return content.replace(/\s*(\-\-)?\s*ngx\.log.*/g,'')
-                                    .replace(/.*getdump.*/g,'');
-                            }
-                        }
+                expand : true,
+                flatten: true,
+                src    : 'src/wiola/wiola.lua',
+                dest   : 'lib/',
+                options: {
+                    process: function (content, srcpath) {
+                        return content.replace(/\s*(\-\-)?\s*ngx\.log.*/g, '')
+                            .replace(/.*getdump.*/g, '');
                     }
-                ]
+                }
+            },
+            lib : {
+                expand : true,
+                cwd    : 'src/wiola/',
+                src    : '**/*.lua',
+                dest   : 'lib/wiola/',
+                options: {
+                    process: function (content, srcpath) {
+                        return content.replace(/\s*(\-\-)?\s*ngx\.log.*/g, '')
+                            .replace(/.*getdump.*/g, '');
+                    }
+                }
             }
         }
     });
